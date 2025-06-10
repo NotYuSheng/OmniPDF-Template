04e93426cc0f47e10325c4d012cc688573f0f86c
junl837 <143344073+junl837@users.noreply.github.com>
2025-06-10 16:47:03 +0800

    feat: basic chat service by running  /c/<query> or /c

---
+++ services/chat/main.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI
+from openai import OpenAI
+from fastapi import FastAPI, Depends, HTTPException, Header
+from pydantic import BaseModel
+from typing import List, Dict
+import uuid
+
+app = FastAPI()
+
+
+@app.get("/health")
+def health_check():
+    return {"status": "ok"}
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, FastAPI!"}
+
+
+app = FastAPI()
+
+
+@app.get("/c")
+def chat():
+    client = OpenAI(
+        base_url="http://localhost:1234/v1",  # Make sure `/v1` is included
+        api_key="lm-studio"  # any dummy string
+    )
+
+    response = client.chat.completions.create(
+        model="qwen2.5-0.5b-instruct",
+        messages=[
+            {"role": "user", "content": "List 3 niche things/items in IT Technical Skills or facts that is useful to a software engineer."}]
+    )
+
+    return {"response": response.choices[0].message.content}
+
+
+@app.get("/c/{chat_item}")
+def chat(chat_item):
+    client = OpenAI(
+        base_url="http://localhost:1234/v1",  # Make sure `/v1` is included
+        api_key="lm-studio"  # any dummy string
+    )
+
+    response = client.chat.completions.create(
+        model="qwen2.5-0.5b-instruct",
+        messages=[{"role": "user", "content": chat_item}]
+    )
+
+    return {"response": response.choices[0].message.content}
