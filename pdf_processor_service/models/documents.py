ab454bab5b6798cafcdb1f7d770db646c9781f5b
Ming Jiu <chiamingjiu@yahoo.com.sg>
2025-06-13 09:33:20 +0800

    fix: Corrected document model

--- pdf_processor_service/models/documents.py
+++
@@ -1,6 +0,0 @@
-def DocumentUploadResponse(doc_id: str, filename: str, download_url: str):
-    return {
-        "doc_id": doc_id,
-        "filename": filename,
-        "download_url": download_url
-    }
