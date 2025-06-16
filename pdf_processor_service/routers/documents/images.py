9e6482286093ab25f4e342b126c3105dd29176f6
Ming Jiu <chiamingjiu@yahoo.com.sg>
2025-06-16 14:18:10 +0800

    feat: change from requests to httpx lib. This allows for async http calls that will not block the server.

--- pdf_processor_service/routers/documents/images.py
+++ pdf_processor_service/routers/documents/images.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Depends, HTTPException, Response
 import logging
 from utils.redis import validate_session_doc_pair
-import requests
 from os import getenv
+from httpx import AsyncClient
 
 router = APIRouter()
 logger = logging.getLogger(__name__)
@@ -11,12 +11,13 @@ logger = logging.getLogger(__name__)
 @router.get("/{doc_id}/images")
 async def get_pdf_images(
     doc_id: str,
+    response: Response,
     valid_request: bool = Depends(validate_session_doc_pair),
-    response=Response,
 ):
     if not valid_request:
         raise HTTPException(status_code=403, detail="Failed to upload file to S3")
-    req = requests.get(getenv("IMAGE_PROCESSER_URL") + f"/{doc_id}")
+    async with AsyncClient() as client:
+        req = await client.get(getenv("IMAGE_PROCESSER_URL") + f"/{doc_id}")
 
-    response.status_code = req.status_code
-    return req.content
+        response.status_code = req.status_code
+        return req.content
--- pdf_processor_service/routers/documents/tables.py
+++ pdf_processor_service/routers/documents/tables.py
@@ -4,8 +4,8 @@ import logging
 from s3_utils import get_file
 from utils.redis import validate_session_doc_pair
 from tempfile import TemporaryFile
-import requests
 from os import getenv
+from httpx import AsyncClient
 
 router = APIRouter()
 logger = logging.getLogger(__name__)
@@ -26,12 +26,13 @@ async def get_pdf_tables_from_s3(
 @router.get("/{doc_id}/tables")
 async def get_pdf_tables(
     doc_id: str,
+    response: Response,
     valid_request: bool = Depends(validate_session_doc_pair),
-    response=Response,
 ):
     if not valid_request:
         raise HTTPException(status_code=403, detail="Not Authorised to access file")
-    req = requests.get(getenv("TABLE_PROCESSER_URL") + f"/{doc_id}")
+    async with AsyncClient() as client:
+        req = await client.get(getenv("IMAGE_PROCESSER_URL") + f"/{doc_id}")
 
-    response.status_code = req.status_code
-    return req.content
+        response.status_code = req.status_code
+        return req.content
