0659e069b05387c6fd330ec9f8e37751c682b692
Ming Jiu <chiamingjiu@yahoo.com.sg>
2025-07-10 21:31:27 +0800

    feat: changed to use logger instead of print feat: redis watcher now runs in seperate thread

--- cleaner_service/utils/cleaner.py
+++ cleaner_service/utils/cleaner.py
@@ -1,10 +1,13 @@
 from collections import defaultdict
+import logging
 
 from redis import Redis
 
 from shared_utils.ttl_utils import CHROMA_FLAG, S3_FLAG, REDIS_FLAG, SEPERATOR
 from shared_utils.redis import config, RedisStringStorage, RedisSetStorage
 from shared_utils.s3_utils import delete_file
+
+logger = logging.getLogger(__name__)
 
 # UNABLE TO HANDLE srem AND OTHERS DUE TO ONLY HAVING EVENT AND KEY INFO
 
@@ -16,18 +19,19 @@ def EMPTY_FUNCTION(x):
     pass
 
 def clean_redis_key(key: str):
-    print("deleting redis key", key)
+    logger.info(f"deleting redis {key}")
     redis_storage = RedisStringStorage()
     del redis_storage[key]
 
 
 def clean_s3_files(key: str):
-    print("deleting s3 key", key)
+    logger.info(f"deleting s3 {key}")
     redis_set_store = RedisSetStorage()
     for doc_id in redis_set_store[key]:
-        doc_id = doc_id.decode()
-        print("deleting" + doc_id)
-        delete_file(doc_id)
+        doc_id = doc_id
+        if doc_id:
+            logger.info(f"deleting {doc_id}")
+            delete_file(f"{doc_id}.pdf")
     del redis_set_store[key]
 
 
@@ -38,6 +42,7 @@ def clean_chromadb(key):
 deletion_prefix_callback_dict = defaultdict(EMPTY_FUNCTION)
 for key, func in {
     S3_FLAG: clean_s3_files,
+    "SessionHeader": clean_s3_files,
     REDIS_FLAG: clean_redis_key,
     CHROMA_FLAG: clean_chromadb,
 }.items():
@@ -45,42 +50,35 @@ for key, func in {
 
 
 def setup_session_expiry_timer(key: str):
+    logger.info(f"Adding expiring for {key}")
     redis_store = RedisStringStorage()
     timer_key = S3_FLAG + ":" + key
-    redis_store[timer_key]
-    redis_store.client.expire(timer_key, 120)
+    redis_store[timer_key] = 1
+    redis_store.client.expire(timer_key, 10)
 
 
 addition_prefix_callback_dict = defaultdict(EMPTY_FUNCTION)
 for key, func in {"Session_Files": setup_session_expiry_timer}.items():
-    addition_prefix_callback_dict[key] = func
+    pass
+    # addition_prefix_callback_dict[key] = func
 
 
 def event_handler(msg):
-    print(
-        "handler --",
-        msg["type"],
-        f"({msg['pattern']})",
-        "from",
-        msg["channel"],
-        ":",
-        msg["data"],
-    )
+    logger.info(f'handler -- {msg["type"]} {msg['pattern']}) from {msg["channel"]}: {msg["data"]}')
     if msg["type"] != "pmessage":
         return
-
-    msg_origin = msg["channel"].decode()
+    msg_origin = msg["channel"]
     if any(event in msg_origin for event in REMOVAL_EVENTS):
-        msg_data: str = msg["data"].decode()
+        msg_data: str = msg["data"]
         flag, key = msg_data.split(SEPERATOR, maxsplit=1)
         # (deletion_prefix_callback_dict[flag])(key)
         deletion_prefix_callback_dict.get(flag, lambda x: None)(key)
 
     if any(event in msg_origin for event in ADDITION_EVENTS):
-        msg_data: str = msg["data"].decode()
+        msg_data: str = msg["data"]
         prefix, key = msg_data.split(SEPERATOR, maxsplit=1)
         # (addition_prefix_callback_dict[prefix])(key)
-        addition_prefix_callback_dict.get(prefix, lambda x: None)(key)
+        addition_prefix_callback_dict.get(prefix, lambda x: None)(msg_data)
     # if "del" in msg_data or "expired" in msg_data:
     #     if REDIS_FLAG in msg_origin:
     #         _, key_to_delete = msg_origin.split(REDIS_FLAG)
@@ -90,7 +88,7 @@ def event_handler(msg):
     #         _, key_to_delete = msg_origin.split(S3_FLAG)
     #         for doc_id in redis_set_store[key_to_delete]:
     #             doc_id = doc_id.decode()
-    #             print("deleting" + doc_id)
+    #             logger.info("deleting" + doc_id)
     #             delete_file(doc_id)
     #         del redis_store[key_to_delete]
     #     elif CHROMA_FLAG in msg_origin:
@@ -98,24 +96,26 @@ def event_handler(msg):
     #         del redis_store[key_to_delete]
 
 
-def setup_redis_watcher():
+async def setup_redis_watcher_thread():
     client = Redis.from_url(config.redis_url)
     # resp = client.config_set("notify-keyspace-events", "KEA")
     client.config_set("notify-keyspace-events", "Egsxn")
     pubsub = client.pubsub()
     sub_key = "__key*__:*"
     pubsub.psubscribe(**{sub_key: event_handler})
-    print(pubsub.patterns)
-    print(pubsub.channels)
-    return pubsub
+    logger.info(pubsub.patterns)
+    logger.info(pubsub.channels)
+    logger.info("Competed setup")
+    return client, pubsub, pubsub.run_in_thread()
 
 
-pubsub = setup_redis_watcher()
-# pubsub.run_in_thread()
-# OR
-print("setup complte")
-for msg in pubsub.listen():
-    pass
+if __name__ == "__main__":
+    pubsub = setup_redis_watcher_thread()
+    # pubsub.run_in_thread()
+    # OR
+    logger.info("setup complte")
+    for msg in pubsub.listen():
+        pass
 
 
 #  {'type': 'psubscribe', 'pattern': None, 'channel': b'__keyevent@*__', 'data': 1}
