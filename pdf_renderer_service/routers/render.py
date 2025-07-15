27fc8123045e6f2173bd97637c43dadd6e8efefb
M00dler <limjjgabriel@gmail.com>
2025-07-15 17:22:00 +0800

    fix: reduced size of output file

--- pdf_renderer_service/routers/render.py
+++ pdf_renderer_service/routers/render.py
@@ -2,6 +2,7 @@ import pymupdf
 import logging
 import io
 import uuid
+import time
 import requests
 from collections import defaultdict
 from fastapi import APIRouter, HTTPException
@@ -19,11 +20,13 @@ logger = logging.getLogger(__name__)
 async def pdf_render(doc_url: str,
                json_url: str):
 
+    start_time = time.time()
+
     response = requests.get(json_url)
     response.raise_for_status()
     json_data = response.json()
 
-    transtext_data = defaultdict(list)
+    trans_text_data = defaultdict(list)
 
     texts = json_data.get("docling", {}).get("texts", [])
     for text_item in texts:
@@ -32,31 +35,26 @@ async def pdf_render(doc_url: str,
             page_no = prov.get("page_no")
             bbox = prov.get("bbox")
             if page_no is not None and bbox:
-                transtext_data[page_no].append({
+                trans_text_data[page_no].append({
                     "translated_text": translated,
                     "bbox": bbox
                 })
-    data = dict(transtext_data)
 
-
-
-    ### Table handler ###
-    trans_table_data = defaultdict(list)
 
     tables = json_data.get("docling", {}).get("tables", [])
     for table in tables:
-        translated = text_item.get("translated_text", "")
-        for prov in text_item.get("prov", []):
-            page_no = prov.get("page_no")
-            bbox = prov.get("bbox")
+        table_cells = table.get("data", {}).get("table_cells", [])
+        page_no = table.get("prov", [])[0].get("page_no")
+        for cell in table_cells:
+            translated = cell.get("translated_text")
+            bbox = cell.get("bbox")
             if page_no is not None and bbox:
-                transtext_data[page_no].append({
+                trans_text_data[page_no].append({
                     "translated_text": translated,
                     "bbox": bbox
                 })
-    data = dict(transtext_data)
 
-
+    data = dict(trans_text_data)
 
     pdf_response = requests.get(doc_url)
     pdf_response.raise_for_status()
@@ -77,15 +75,34 @@ async def pdf_render(doc_url: str,
         for trans_data in data_lst:
             trans_text = trans_data["translated_text"]
             bbox = trans_data['bbox']
-            coords = (bbox["l"], (page.rect[3]- bbox["t"]), bbox["r"], page.rect[3]- bbox["b"])
-            page.insert_htmlbox(coords, trans_text)
-            
+
+            y0 = (page.rect[3]- bbox["t"])
+            y1 = (page.rect[3]- bbox["b"])
+
+            top = min(y0, y1)
+            bottom = max(y0, y1)
+
+            coords = (bbox["l"], top, bbox["r"], bottom)
+
             logger.info(f"Text: {trans_text}")
             logger.info(f"Bbox: {coords}")
+            try:
+                page.insert_htmlbox(coords, trans_text)
+            except Exception as e:
+                logger.error("Error inserting HTML box:")
+                logger.error(f"Text: {trans_text}")
+                logger.error(f"Original BBox: {bbox}")
+                logger.error(f"Converted Coords: {coords}")
+                logger.error(f"Page size: {page.rect}")
+                logger.exception(e)  # full traceback
+                raise e  # optionally re-raise for FastAPI to return 500
+
             
     buffer = io.BytesIO()
     doc.save(buffer, garbage=3, deflate=True)
     buffer.seek(0)  # Reset buffer position
+
+    logger.info(f"Time to render document: {time.time() - start_time}")
 
     doc_id = str(uuid.uuid4())
     key = f"{doc_id}/rendered.pdf"
