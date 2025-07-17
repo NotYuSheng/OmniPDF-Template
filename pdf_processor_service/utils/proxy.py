611512add045a01b779f2252585b9753f62b09c1
Ming Jiu <chiamingjiu@yahoo.com.sg>
2025-07-17 15:30:36 +0800

    feat: shifted EXTERNAL_S3_ENDPOINT to  .env

--- pdf_processor_service/utils/proxy.py
+++ pdf_processor_service/utils/proxy.py
@@ -1,7 +1,9 @@
 import os
 
 S3_ENDPOINT = os.getenv("MINIO_ENDPOINT", "http://minio:9000")  # MinIO-compatible
-EXTERNAL_S3_ENDPOINT = "http://localhost:8080/file"
+EXTERNAL_S3_ENDPOINT = os.getenv("EXTRACTION_URL")
+if not EXTERNAL_S3_ENDPOINT:
+    raise ValueError("EXTERNAL_S3_ENDPOINT is not set")
 
 def get_external_minio_uri(uri: str):
     new_uri = uri.replace(S3_ENDPOINT, EXTERNAL_S3_ENDPOINT)
