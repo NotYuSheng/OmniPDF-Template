94d9548d7420443074d4f22aa08ffeec0dcd3716
Ming Jiu <chiamingjiu@yahoo.com.sg>
2025-07-17 14:48:32 +0800

    feat: now follow images and creates job if there is none

--- pdf_processor_service/routers/tables.py
+++ pdf_processor_service/routers/tables.py
@@ -1,13 +1,15 @@
 import logging
 from os import getenv
+from urllib.parse import urlencode
 
-from fastapi import APIRouter, Depends, HTTPException, Response
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.responses import JSONResponse
 
 from models.tables import TablesResponse
-from utils.asynchttp import proxy_get, proxy_post
+from utils.asynchttp import proxy_post
 from shared_utils.s3_utils import generate_presigned_url
 from utils.session import validate_session_doc_pair
-from shared_utils.redis import get_set_storage, RedisSetStorage
+from shared_utils.s3_utils import load_job
 
 router = APIRouter(prefix="/tables", tags=["tables"])
 logger = logging.getLogger(__name__)
@@ -16,30 +18,30 @@ if not TABLE_PROCESSOR_URL:
     raise ValueError("TABLE_PROCESSOR_URL is not set")
 
 
-@router.get("/{doc_id}", response_model=TablesResponse)
+@router.get("/{doc_id}")
 async def get_pdf_tables(
     doc_id: str,
-    response: Response,
     valid_request: bool = Depends(validate_session_doc_pair),
-    service_cache: RedisSetStorage = Depends(get_set_storage),
 ):
     if not valid_request:
         raise HTTPException(
             status_code=403,
             detail="User not authorized to access this document or invalid document ID",
         )
-    doc_is_processing = service_cache.contains(__name__, doc_id)
-    if not doc_is_processing:
-        download_url = generate_presigned_url(f"{doc_id}.pdf")
-        req = await proxy_post(
-            f"{TABLE_PROCESSOR_URL}",
-            body={"doc_id": doc_id, "download_url": download_url},
+    job = load_job(doc_id=doc_id, job_type="extraction")
+    if not job:
+        presign_url = generate_presigned_url(f"{doc_id}/original.pdf")
+        param = {"doc_id": doc_id, "download_url": presign_url}
+        return await proxy_post(f"{TABLE_PROCESSOR_URL}?{urlencode(param)}", body=None)
+    
+    if job.get("status") == "processing":
+        raise HTTPException(
+            status_code=202,
+            detail="The document is still being processed. Please try again later."
         )
-    else:
-        req = await proxy_get(f"{TABLE_PROCESSOR_URL}/{doc_id}")
-    if req.status_code == 202 and not doc_is_processing:
-        service_cache.add(__name__, doc_id)
-    elif req.status_code == 200 and doc_is_processing:
-        service_cache.remove(__name__, doc_id)
-    response.status_code = req.status_code
-    return req.json()
+
+    try:
+        return JSONResponse(content=job.get("data").get("result").get("tables"))
+    except AttributeError as e:
+        logger.error(e)
+        raise HTTPException(status_code=500, detail="A Server Error has occured")
